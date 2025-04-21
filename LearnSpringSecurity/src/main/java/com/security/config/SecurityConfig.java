48af9720b21f77fe59b3956c1447c5f4848c45c2
Nivan <Nivan@DESKTOP-FKSC388>
2025-04-22 01:00:48 +0530

    Implemented InMemoryUserDetailsManager

--- LearnSpringSecurity/src/main/java/com/security/config/SecurityConfig.java
+++ LearnSpringSecurity/src/main/java/com/security/config/SecurityConfig.java
@@ -4,33 +4,63 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
-
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-		
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
 		http.authorizeHttpRequests((requests) -> requests
 				// This permit to contact
-				
-				//.requestMatchers("/contact").permitAll()
-				.anyRequest()
-				.authenticated());
+
+				// .requestMatchers("/contact").permitAll()
+				.anyRequest().authenticated());
 		http.csrf(csrf -> csrf.disable());
-		//http.formLogin(withDefaults());
+		// http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		// This will hide the cookies
-		//http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-		
+		// http.sessionManagement(session ->
+		// session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
 		return http.build();
+
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 		
+//		if (!manager.userExists("user1")) {
+//			manager.createUser(User.withUsername("user1").password("{noop}password").roles("USER").build());
+//
+//		}
 		
+		//Alternate way 
+		
+		UserDetails userDetails = User.withUsername("user1")
+				.password("{noop}password")
+				.roles("USER")
+				.build();
+		if (!manager.userExists("user1")) {
+			System.out.println("hello");
+		manager.createUser(userDetails);
+
+	}
+		
+
+		if (!manager.userExists("admin")) {
+			manager.createUser(User.withUsername("admin").password("{noop}adminpass").roles("ADMIN").build());
+
+		}
+		return manager;
 	}
 
 }
